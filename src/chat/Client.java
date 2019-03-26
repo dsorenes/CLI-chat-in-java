1cd648c92479e9508871b4964a23d37f7565d40b
Daniel Sørenes <Danielsorenes@hotmail.no>
2019-03-26 01:34:29 +0100

    first commit

---
+++ src/chat/Client.java
@@ -0,0 +1,42 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+   private String hostname;
+   private int port;
+   protected Socket client;
+
+    public Client(String hostname, int port) {
+
+       this.hostname = hostname;
+       this.port = port;
+       try {
+           this.client = new Socket(this.hostname, this.port);
+           PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+           BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+           BufferedReader stdInput = new BufferedReader(new InputStreamReader(System.in));
+
+           String message;
+
+           while ((message = stdInput.readLine()) != null) {
+               //this sends the client input to the server
+               output.println(message);
+
+               //this prints out what we get from the server to the client locally
+               System.out.println(input.readLine());
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+
+    public static void main (String[] args) {
+        new Client("localhost", 59000);
+    }
+}
---
+++ src/chat/Server.java
@@ -0,0 +1,45 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    protected ServerSocket server;
+    protected int port;
+
+    public Server(int port) {
+       this.port = port;
+
+       try {
+
+           this.server = new ServerSocket(port);
+           System.out.println("Server is running...");
+           Socket client = server.accept();
+           PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+           BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+           if (client.isConnected()) {
+               System.out.println("client connected");
+           }
+           String message;
+
+           //the message is what is received from the client
+           while ((message = input.readLine()) != null) {
+               //this prints the client message locally to the server
+               System.out.println(message);
+               //this sends the client message back to the client
+               output.println("Server: " + message);
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+
+    public static void main(String[] args) {
+        new Server(59000);
+    }
+}
