ee6d13aa6fc6ce34307a1e9771173bae0e6e4754
Daniel Sørenes <Danielsorenes@hotmail.no>
2019-03-28 03:58:51 +0100

    Server now supports multiple clients, communication between them still a work-in-progress

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -6,39 +6,61 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server {
 
     protected ServerSocket server;
     protected int port;
+    protected Set<ClientHandler> clients = new HashSet<>();
+    protected MessageBroadcaster broadcaster;
+    private Set<ClientHandler> disconnected = new HashSet<>();
 
     public Server(int port) {
-       this.port = port;
+        this.port = port;
 
-       try (
-           var server = new ServerSocket(this.port)
-       ){
+        try (
+                var server = new ServerSocket(this.port)
+        ) {
 
-           this.server = server;
-           System.out.println("Server is running...");
-           Socket client = server.accept();
-           PrintWriter output = new PrintWriter(client.getOutputStream(), true);
-           BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-           if (client.isConnected()) {
-               System.out.println("client connected");
-           }
-           String message;
+            this.server = server;
+            System.out.println("Server is running...");
 
-           //the message is what is received from the client
-           while ((message = input.readLine()) != null) {
-               //this prints the client message locally to the server
-               System.out.println(message);
-               //this sends the client message back to the client
-               output.println("Server: " + message);
-           }
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
+            while (true) {
+                ClientHandler client = new ClientHandler(this.server.accept());
+                new Thread(client).start();
+                synchronized (clients) {
+                    clients.add(client);
+                    System.out.println("Clients connected " + clients.size());
+                }
+
+                for (ClientHandler c : clients) {
+                    if (c.isConnected() == false) disconnected.add(c);
+
+                }
+
+                synchronized (clients) {
+                    clients.removeAll(disconnected);
+                    disconnected.clear();
+                }
+
+                System.out.println("before " + disconnected.size());
+
+
+
+
+                System.out.println("after " + disconnected.size());
+                System.out.println("Clients connected " + clients.size());
+
+                broadcaster = new MessageBroadcaster(this.clients);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
